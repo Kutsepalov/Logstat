1495101dc77847ee529b6120ff6258d4e86b552f
Revan1313 <romagrabovetskiy@gmail.com>
2021-10-31 16:11:41 +0200

    CollectorFactory added

---
+++ src/main/java/com/softserve/logstat/service/collector/CollectorFactory.java
@@ -0,0 +1,15 @@
+package com.softserve.logstat.service.collector;
+
+public class CollectorFactory {
+    public static Collector choose(String str){
+        try {
+            if (str == "top") return new CollectorTop();
+            else if (str == "stat") return new CollectorStat();
+            else if (str == "find") return new CollectorFind();
+            else {throw new IllegalArgumentException();}
+        }catch (IllegalArgumentException e){
+            System.out.println("Wrong flag entered");
+        }
+        return null;
+    }
+}
