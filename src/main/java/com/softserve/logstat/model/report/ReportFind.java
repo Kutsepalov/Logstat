c169013bba15c078a5ad59d1830301df25ed00e4
Max <kutsepalov.maks.work@gmail.com>
2021-10-31 09:34:03 +0200

    Add empty classes for developing. Create new dirs

---
+++ src/main/java/com/softserve/logstat/model/report/ReportFind.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright 2021
+ *
+ * All rights reserved.
+ *
+ * Created on Oct 29, 2021 3:32:54 PM
+ */
+package com.softserve.logstat.model.report;
+
+/**
+ * @author <paste here your name>
+ *
+ */
+public class ReportFind implements Report {
+
+    @Override
+    public String getAsString() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+    
+}
