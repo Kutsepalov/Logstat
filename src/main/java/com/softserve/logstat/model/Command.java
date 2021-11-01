5c86b2359090f1bc87dc6e6957d3a9aa1e5146ad
Max <kutsepalov.maks.work@gmail.com>
2021-11-01 17:21:55 +0200

    Refactor code

--- src/main/java/com/softserve/logstat/model/Command.java
+++ src/main/java/com/softserve/logstat/model/Command.java
@@ -1,27 +1,15 @@
 package com.softserve.logstat.model;
 
-
-import java.util.ArrayList;
-
 /**
  * @author Roman Grabovetskyi
  *  mock class
  */
-
-
-
-
 public class Command {
 
     int limit;
     enum ParamTime{}
-/*List<T> Predicate;
-Список предикатов. Пока не реализован
-* */
+    /**List<T> Predicate;
+    	Список предикатов. Пока не реализован
+    */
     String collectorType;
-
-
-
-
-
 }
