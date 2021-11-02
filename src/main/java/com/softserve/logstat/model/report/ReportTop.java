d2a6df02b5426bb6572997ce7329fa9fc6b93ed8
rednikeeg <maks.schoolforvv@gmail.com>
2021-11-02 17:28:23 +0200

    ReportTop implemented, changed String to List<String> and getAsString() to getAsList()

--- src/main/java/com/softserve/logstat/model/report/ReportTop.java
+++ src/main/java/com/softserve/logstat/model/report/ReportTop.java
@@ -1,22 +1,62 @@
-/**
- * Copyright 2021
- *
- * All rights reserved.
- *
- * Created on Oct 29, 2021 3:32:54 PM
- */
 package com.softserve.logstat.model.report;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 /**
- * @author <paste here your name>
- *
+ * @author Bohachov Maksym
  */
 public class ReportTop implements Report {
+    private final Map<String, Integer> info;
+    private int length;
+
+    /**
+     * Creates instance of ReportTop
+     * @param info - input map
+     */
+    public ReportTop(Map<String, Integer> info) {
+        this.info = info;
+
+        length = 0;
+
+        if (!info.isEmpty()) {
+            checkLength();
+        }
+    }
+
+    /**
+     * Computes length of column based on max length of keySet's elements
+     */
+    private void checkLength() {
+        Iterator<String> iterator = info.keySet().iterator();
+
+        length = iterator.next().length();
+
+        while(iterator.hasNext()) {
+            length = Math.max(iterator.next().length(), length);
+        }
+    }
 
     @Override
-    public String getAsString() {
-	// TODO Auto-generated method stub
-	return null;
+    public List<String> getAsList() {
+        List<String> result = new ArrayList<>(info.size() + 1);
+
+        if(length > 0) {
+            Iterator<String> iterator = info.keySet().iterator();
+
+            result.add(String.format("| %-" + length + "s | %-9s |", "Key", "Value"));
+
+            while (iterator.hasNext()) {
+                String current = iterator.next();
+
+                result.add(String.format("| %-" + length + "s | %-9d |", current, info.get(current)));
+            }
+        } else {
+            result.add("There is no information.");
+        }
+
+        return result;
     }
-    
 }
