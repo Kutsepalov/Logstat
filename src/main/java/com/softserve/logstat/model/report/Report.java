5bc159b54f3bb77ed59e716529b59b9519226540
DEEVERee <dbrbyu369@gmail.com>
2021-11-02 19:30:14 +0200

    Change JDK version to 15. Implement of ReportStat and ReportWriter classes.

--- src/main/java/com/softserve/logstat/model/report/Report.java
+++ src/main/java/com/softserve/logstat/model/report/Report.java
@@ -1,12 +1,14 @@
 /**
  * Copyright 2021
- *
+ * <p>
  * All rights reserved.
- *
+ * <p>
  * Created on Oct 29, 2021 2:08:47 PM
  */
 package com.softserve.logstat.model.report;
 
+
+import java.util.List;
 
 import java.util.List;
 
--- src/main/java/com/softserve/logstat/service/ReportWriter.java
+++ src/main/java/com/softserve/logstat/service/ReportWriter.java
@@ -1,45 +1,82 @@
 /**
  * Copyright 2021
- *
+ * <p>
  * All rights reserved.
- *
+ * <p>
  * Created on Oct 29, 2021 3:21:03 PM
  */
 package com.softserve.logstat.service;
 
 import com.softserve.logstat.model.report.Report;
+import com.softserve.logstat.model.report.ReportStat;
+
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 
 /**
- * @author <paste here your name>
- *
+ * @author <Dmitriy Veretelnikov>
  */
 public class ReportWriter {
-    private String filePath;
-    
+    private static boolean firstLine = true;
+    private String fileOutput;
+
+    public String getFileOutput() {
+        return fileOutput;
+    }
+
+    public void setFileOutput(String fileOutput) {
+        this.fileOutput = fileOutput;
+    }
+
     public ReportWriter() {
-	
-    }
-    
-    public ReportWriter(String filePath) {
-	this.filePath = filePath;
-    }
-    
-    public void write(Report report) {
-	
     }
 
-    /**
-     * @return the filePath
-     */
-    public String getFilePath() {
-        return filePath;
+    public ReportWriter(String fileOutput) {
+        this.fileOutput = fileOutput;
     }
 
-    /**
-     * @param filePath the filePath to set
-     */
-    public void setFilePath(String filePath) {
-        this.filePath = filePath;
+    public void write(Report report) throws IOException {
+        BufferedWriter bw = null;
+        if (getFileOutput() != null) {
+            try {
+                bw = new BufferedWriter(new FileWriter(getFileOutput()));
+                for (String line : report.getAsList()) {
+                    if (firstLine) {
+                        bw.write(line);
+                        firstLine = false;
+                    } else {
+                        bw.write("\n" + line);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                bw.close();
+            }
+        } else {
+            for (String line : report.getAsList()) {
+                System.out.println(line);
+            }
+        }
     }
+
+    public static void main(String[] args) throws IOException {
+        ReportStat rep = new ReportStat();
+        Map<String, Integer> map = new LinkedHashMap<>();
+//        map.put("83.149.9.216", 10);
+//        map.put("24.236.252.67", 32);
+//        map.put("50.16.19.13", 4);
+//        map.put("110.136.166.128", 154);
+//        map.put("110.12.166.128", 154);
+        rep.setStatRes(map);
+        ReportWriter rw = new ReportWriter("output.txt");
+        rw.write(rep);
+    }
+
 
 }
