8d6ca9a2f6c74f96ef9a52bb774d61157aa9c0ad
maksimvidal <silchenkko08@gmail.com>
2021-11-03 16:23:36 +0200

    collectorFind appearance has impoved

--- src/main/java/com/softserve/logstat/service/collector/CollectorFind.java
+++ src/main/java/com/softserve/logstat/service/collector/CollectorFind.java
@@ -1,6 +1,5 @@
 package com.softserve.logstat.service.collector;
 
-
 import com.softserve.logstat.model.Log;
 import com.softserve.logstat.model.report.Report;
 import com.softserve.logstat.model.report.ReportFind;
@@ -12,27 +11,28 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
- * @author Roman Grabovetskyi
- *  mock class
+ * @author Roman Grabovetskyi mock class
  */
 public class CollectorFind extends CollectorTop {
 
 	@Override
-	public Report collect(Stream<Log> logs,Command command) {
-		List<Log> logslist=new ArrayList<>(logs.collect(Collectors.toList()));
-		var wrapper=new Object() {int index=0;}; 
-		var result= command.getToWrite().
-				stream().
-				map(type -> logslist.
-						stream().
-						filter(command.getToFind()).
-						map(log -> getFieldByParam(type, log).toString()).
-						distinct().
-				        collect(Collectors.toList())).
-				collect(toMap(
-						line -> command.getToWrite().get(wrapper.index++).toString(),
-						line -> line));
+	public Report collect(Stream<Log> logs, Command command) {
+		List<Log> logslist = new ArrayList<>(logs.collect(Collectors.toList()));
+		var wrapper = new Object() {
+			int index = 0;
+		};
 		
+		var result = command.getToWrite()
+				.stream()
+				.map(type -> logslist.stream()
+						.filter(command.getToFind())
+				        .map(log -> getFieldByParam(type, log).toString())
+				        .distinct()
+				        .collect(Collectors.toList()))
+				.collect(toMap(
+			            line -> command.getToWrite().get(wrapper.index++).toString(), 
+						line -> line));
+
 		return new ReportFind(result);
 	}
 }
